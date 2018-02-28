c5be7d461396922b72200b8926a54f83d4095a18
Kelsi Summers <kelsisummers@Kelsis-MacBook-Air.local>
2018-02-28 16:20:04 -0700

    connecting js files

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,20 @@
-var friends = [
-  {
-  name: "Ahmed",
-  photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-  scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
-  }
-];
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+var friends = require('../data/friends.js');;
 
-console.log(userData);
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// console.log(userData);
 
 // Get All Reservations
 app.get("/api/friends", function(req, res) {
@@ -19,5 +27,11 @@ app.post("/api/:new", function(req, res) {
   // This Works Because Of The Body-Parser Middleware
   var newFriend = req.body;
 
-  newFriend.push(friends);
+  friend.push(newFriend);
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
 });
