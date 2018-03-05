cd57b7dd246ec6fa05978c7e15fb40b36d4f7b7a
Kelsi Summers <kelsisummers@Kelsis-MacBook-Air.local>
2018-03-04 17:17:23 -0700

    css

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
+app.use(express.static(__dirname + '/public'));
 
 // Routing
 app.use(htmlRoute);
